1036305d495ec7cfc551108d23be35112b5eb03a
jguers <jguers@4ddc36ad-14ed-0310-aede-e2e70885ca97>
2005-02-25 00:04:28 +0000

    La defaite : la communication avec la carte ne marche plus ?? J'abandonne pour ce soir

--- trunk/test/de/berlios/jmds/applet/SCAppletWithKey.java
+++ trunk/test/de/berlios/jmds/applet/SCAppletWithKey.java
@@ -9,7 +9,10 @@ import javacard.framework.APDU;
 import javacard.framework.Applet;
 import javacard.framework.ISOException;
 import javacard.framework.ISO7816;
-import javacard.security.RandomData;
+import javacard.security.KeyBuilder;
+import javacard.security.RSAPrivateKey;
+import javacard.security.RSAPublicKey;
+//import javacardx.crypto.*;
 
 /**
  * DOCME
@@ -18,33 +21,35 @@ import javacard.security.RandomData;
  * 
  * @author Jérôme GUERS
  */
-public class SCApplet extends Applet {
+public class SCAppletWithKey extends Applet {
 
     private final static byte CLA_SECURITY = (byte) 0x69;
-    
-    private final static byte INS_CODE = (byte) 0x10;
-    private final static byte INS_GET_CODE = (byte) 0x11;
-    private final static byte INS_DECODE = (byte) 0x30;
-    private final static byte INS_GET_DECODE = (byte) 0x33;
-    private final static byte INS_SET_USER_KEY = (byte) 0x77;
-    
-    private final static short BUFFER_LENGTH = (short) 0x20;
+    private final static byte CODE = (byte) 0x10;
+    private final static byte GET_CODE = (byte) 0x20;
+    private final static byte DECODE = (byte) 0x30;
+    private final static byte GET_DECODE = (byte) 0x40;
+    private final static short BUFFER_LENGTH = (short) 255;
 
-    private final RandomData randomer;
-    
     private byte[] buffer;
     private byte[] code;
-    private byte key;
-
+    private short messLength;
+    
+    private RSAPrivateKey userKey;
+    private RSAPublicKey serverKey;
+    private RSAPrivateKey sessionKey;
+    //private Cipher cipher;
     
     /**
      * Constructeur par défaut
      */
-    protected SCApplet() {
+    protected SCAppletWithKey() {
         buffer = new byte[BUFFER_LENGTH];
         code = new byte[BUFFER_LENGTH];
-        randomer = RandomData.getInstance(RandomData.ALG_SECURE_RANDOM);
-        key = (byte)0;
+        messLength = (byte) 0;
+        userKey = (RSAPrivateKey) KeyBuilder.buildKey(KeyBuilder.TYPE_RSA_PRIVATE, KeyBuilder.LENGTH_RSA_512, true);
+        serverKey = (RSAPublicKey) KeyBuilder.buildKey(KeyBuilder.TYPE_RSA_PUBLIC, KeyBuilder.LENGTH_RSA_512, true);
+        sessionKey = (RSAPrivateKey) KeyBuilder.buildKey(KeyBuilder.TYPE_RSA_PRIVATE, KeyBuilder.LENGTH_RSA_512, true);
+        //cipher = Cipher.getInstance(Cipher.ALG_RSA_ISO9796, false);
         register();
     }
 
@@ -52,7 +57,7 @@ public class SCApplet extends Applet {
      * @see javacard.framework.Applet#install(byte[], short, byte)
      */
     public static void install(byte[] bArray, short bOffset, byte bLength) {
-        new SCAppletWithKey();
+        new SCApplet();
     }
 
     /**
@@ -66,49 +71,41 @@ public class SCApplet extends Applet {
     public void process(APDU apdu) throws ISOException {
         byte[] buffer = apdu.getBuffer();
 
+        // CLA à 69
         if (buffer[ISO7816.OFFSET_CLA] == CLA_SECURITY) {
             switch (buffer[ISO7816.OFFSET_INS]) {
-                case INS_CODE:
+                case CODE:
                     encode(apdu);
                     break;
                 
-                case INS_GET_CODE:
+                case GET_CODE:
                     getEncode(apdu);
                     break;
 
-                case INS_DECODE:
+                case DECODE:
                     break;
                     
-                case INS_GET_DECODE:
+                case GET_DECODE:
                     break;
-                    
-                case INS_SET_USER_KEY:
-                    break;
-                    
-                default:
-                    ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
             }
         }
-        else
-            ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
     }
     
     private void encode(APDU apdu)
     {
-        short messLength = apdu.setIncomingAndReceive();
-        randomer.generateData(code, (short) 0, BUFFER_LENGTH);
+        messLength = apdu.setIncomingAndReceive();
+        byte[] requestID = new byte[messLength];
         for (short i = 0; i < messLength; i++) {
-            code[(short)(i + key)] = buffer[(short)(i + ISO7816.OFFSET_CDATA)];
+            requestID[i] = (byte)(buffer[(short)(i + ISO7816.OFFSET_CDATA)] & 0x10);
         }
+        //cipher.init(userKey, Cipher.MODE_ENCRYPT);
     }
     
     private void getEncode(APDU apdu)
     {
-        short Le = apdu.setOutgoing();
-        // Verification que Le >= Lc (taille des données envoyées)
-        if (Le < BUFFER_LENGTH)
+        if(apdu.setOutgoing() < messLength)
             ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
-        apdu.setOutgoingLength(BUFFER_LENGTH);
-        apdu.sendBytesLong(code, (short) 0, BUFFER_LENGTH);
+        
+        apdu.sendBytesLong(code, (short)0, messLength);
     }
 }
